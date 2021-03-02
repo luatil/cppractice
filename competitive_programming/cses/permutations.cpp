176a8f29a38915ad4d3d7a100af6f563ec531608
Luã Santilli <luasantilli@gmail.com>
2021-03-02 10:08:44 -0300

    Fix spacing issues
    Signed-off-by: Luã Santilli <luasantilli@gmail.com>

--- competitive_programming/cses/permutations.cpp
+++ competitive_programming/cses/permutations.cpp
@@ -10,9 +10,9 @@ void print_subtracting_two(int n)
 
 void beautiful_permutation(int n)
 {
-    if(n==1)   std::cout << "1";
+    if(n==1)        std::cout << "1";
     else if(n < 4)  std::cout << "NO SOLUTION";
-    else if(n==4) std::cout << "2 4 1 3";
+    else if(n==4)   std::cout << "2 4 1 3";
     else 
     {
         // This evens/odds or odds/evens combinations works
