71642a5f03addf59560e838e345d48501aaae928
Luã <luasantilli@gmail.com>
2021-03-02 10:57:07 -0300

    Update permutations.cpp

--- competitive_programming/cses/permutations.cpp
+++ competitive_programming/cses/permutations.cpp
@@ -1,27 +1,39 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <iterator>
+#include <iostream>
 
-std::string get_reversed_order_subtracting_two(const int n)
+auto get_reversed_order_subtracting_two(const int n) -> std::vector<int>
 {
-    std::string reversed;
+    std::vector<int> reversed;
     reversed.reserve(n/2+1); // That's a mouthfull
     for(int i = n; i > 0; i-= 2)
         reversed.push_back(i);
     return reversed;
 }
 
-std::string beautiful_permutation(const int n)
+auto beautiful_permutation(const int n) -> std::string
 {
     if(n == 1) return "1\n";
     if(n < 4)  return "NO SOLUTION\n";
-    return get_reversed_order_subtracting_two(n-1);
+    auto v1 = get_reversed_order_subtracting_two(n-1);
+    auto v2 = get_reversed_order_subtracting_two(n);
+
+    std::ostringstream vts;
+    std::copy(v1.begin(), v1.end(), std::ostream_iterator<int>(vts, " "));
+    std::copy(v2.begin(), v2.end()-1, std::ostream_iterator<int>(vts, " "));
+    // Add the element with no delimiter
+    vts << v2.back();
+
+    return vts.str();
 }
 
 int main()
 {
     int n; 
     std::cin >> n;
-    // std::string solution = beautiful_permutation(n);
-    std::string solution = get_reversed_order_subtracting_two(n);
-    std::cout << solution;
+    std::cout << beautiful_permutation(n) << std::endl;
 }
